de329fb60a7e85b9b2f05f2d3ce17eee56a14cc9
Hui Chen <achen509@ca.nam.ad.pwcinternal.com>
2019-01-21 10:20:53 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SearchResult from './SearchResult';
+import Search from './Search';
+
+class App extends Component {
+  
+  state = {
+      twlData: [],
+      searchKey:""
+};
+
+componentDidMount() {
+  const url = "https://secure.toronto.ca/cc_sr_v1/data/swm_waste_wizard_APR?limit=1000";
+
+  fetch(url)
+      .then(dataResult => dataResult.json())
+      .then(result => {
+        this.state.twlData = result;
+      });
+}
+
+toggleFavourite = title => {
+      let result = this.state.twlData;
+      let index = result.findIndex((item) =>{
+        return item.title === title;
+      }); 
+      if (result[index].IsFavourite){
+        result[index].IsFavourite = false;
+      }
+      else{
+        result[index].IsFavourite = true;
+      }
+      this.setState({twlData: result});
+  };
+
+  handleSubmit = (search) => {
+    let  result = search.searchKey;
+    this.setState({searchKey: result});
+}
+
+  render() {
+    const source = this.state.twlData;
+    const searchKey = this.state.searchKey;
+    const favourites = source.filter((item) => { return item.IsFavourite; });
+
+    let lookupResult = [];
+    if (searchKey.length > 0)
+      lookupResult = source.filter((item)=>{
+            return item.title.toLowerCase().indexOf(searchKey.toLowerCase()) !== -1;
+         });
+
+    return (
+      <div className="app">
+        <div className="App-header">
+          Toronto Waste Lookup
+        </div>
+        <div className="container">
+        <Search  handleSearchSubmit={this.handleSubmit} />
+
+        <SearchResult 
+          searchResult = {lookupResult} 
+          toggleFavourite = {this.toggleFavourite} 
+        />
+        
+        <div className={`${favourites.length>0? "visible":"invisible" }`} ><h3 className="text-success">Favourites</h3></div>
+        
+        <SearchResult 
+          searchResult = {favourites} 
+          toggleFavourite = {this.toggleFavourite} 
+        />
+         </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Search.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+
+class Search extends Component {
+
+    constructor(props) {
+        super(props);
+        
+        this.initialState = {
+            searchKey: ''
+        };
+
+        this.state = this.initialState;
+    
+    }
+
+    handleChange = event => {
+        this.setState({searchKey : event.target.value});
+        if (event.target.value === ""){
+            this.props.handleSearchSubmit({searchKey : ""});
+        }
+    }
+
+    submitForm = (event) => {
+        event.preventDefault();
+        this.props.handleSearchSubmit(this.state);
+    }
+
+    render() {
+        const {searchKey} = this.state;
+        return(
+            <div className="pt-4">
+                <form onSubmit={this.submitForm}>
+                    <div className="row">
+                        <div className="col-md-12">
+                            <div className="input-group input-group-lg">
+                                <input type="text" name="searchKey" className="form-control" value={searchKey} 
+                                    onChange={this.handleChange} />
+                                <button type="button" className="btn btn-success">
+                                    <span className="glyphicon glyphicon-search" onClick={this.submitForm}></span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Search;
